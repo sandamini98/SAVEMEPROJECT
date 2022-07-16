afe9572675aecb7be59996bddabd352f8f4b7cb4
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-16 18:22:44 +0530

    create addBlog function

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -11,4 +11,57 @@ export const getAllBlogs = async(req,res,next) => {
         return res.status(404).json({message:"No Blogs Found"})
     }
     return res.status(200).json({blogs})
-}
+};
+
+export const addBlog = async(req,res,next) => {
+    const {title, description,image,user} = req.body;
+    const blog = new Blog({
+        title,
+        description,
+        image,
+        user,
+    });
+    try {
+        await blog.save()
+    }catch(err) {
+        return console.log(err)
+    }
+    return  res.status(200).json({blog})
+};
+
+
+
+/*
+import Blog from "../model/Blog";
+
+export const getAllBlogs = async (req, res, next) => {
+  let blogs;
+  try {
+    blogs = await Blog.find().populate("user");
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!blogs) {
+    return res.status(404).json({ message: "No Blogs Found" });
+  }
+  return res.status(200).json({ blogs });
+};
+
+export const addBlog = async (req, res, next) => {
+  const { title, description, image, user } = req.body;
+
+  const blog = new Blog({
+    title,
+    description,
+    image,
+    user,
+  });
+  try {
+    await blog.save();
+  } catch (err) {
+    return console.log(err)
+  }
+
+  return res.status(200).json({ blog });
+};
+*/
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,7 +1,12 @@
+
 import express from 'express';
-import { getAllBlogs } from '../controllers/blog-controller';
+import { addBlog, getAllBlogs } from '../controllers/blog-controller';
 const blogRouter = express.Router();
 
-blogRouter.get("/",getAllBlogs);
+blogRouter.get("/", getAllBlogs);
+blogRouter.post("/add", addBlog);
 
 export default blogRouter;
+
+
+
