61db5a83e652b123053db2da99b5b2d778173578
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-16 18:51:55 +0530

    add updateBlog function

--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,10 +1,11 @@
 
 import express from 'express';
-import { addBlog, getAllBlogs } from '../controllers/blog-controller';
+import { addBlog, getAllBlogs, updateBlog } from '../controllers/blog-controller';
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add", addBlog);
+blogRouter.put("/update/:id", updateBlog);
 
 export default blogRouter;
 
