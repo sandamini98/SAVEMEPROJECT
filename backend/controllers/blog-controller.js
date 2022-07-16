b87770b733a658c6068008312472f0264a9a724f
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-16 19:11:19 +0530

    create getById function in the backend

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -48,3 +48,18 @@ export const updateBlog = async (req, res, next) => {
   };
 
 
+export const getById = async (req, res, next) => {
+    const id = req.params.id;
+    let blog;
+    try {
+      blog = await Blog.findById(id);
+    } catch (err) {
+      return console.log(err);
+    }
+    if (!blog) {
+      return res.status(404).json({ message: "No Blog Found" });
+    }
+    return res.status(200).json({ blog });
+  };
+
+
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,11 +1,12 @@
 
 import express from 'express';
-import { addBlog, getAllBlogs, updateBlog } from '../controllers/blog-controller';
+import { addBlog, getAllBlogs, getById, updateBlog } from '../controllers/blog-controller';
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add", addBlog);
 blogRouter.put("/update/:id", updateBlog);
+blogRouter.get("/:id",getById);
 
 export default blogRouter;
 
