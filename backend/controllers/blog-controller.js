b46806b6681ea7e3f1c9b0e156e2006571b50803
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-16 19:25:14 +0530

    create deleteBlog function in the backend

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -62,4 +62,21 @@ export const getById = async (req, res, next) => {
     return res.status(200).json({ blog });
   };
 
+export const deleteBlog = async (req, res, next) => {
+    const id = req.params.id;
+  
+    let blog;
+    try {
+      blog = await Blog.findByIdAndRemove(id).populate("user");
+      await blog.user.blogs.pull(blog);
+      await blog.user.save();
+    } catch (err) {
+      console.log(err);
+    }
+    if (!blog) {
+      return res.status(500).json({ message: "Unable To Delete" });
+    }
+    return res.status(200).json({ message: "Successfully Delete" });
+  };
+
 
