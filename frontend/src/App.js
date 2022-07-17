674df7134955994a610d01d820791821fd474742
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-17 11:33:34 +0530

    add name, login and signup button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div >
-      HELLO WORLD
-    </div>
-  );
+  return <Header/>;
 }
 
 export default App;
+
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import {AppBar, Toolbar, Typography, Box, Button} from '@mui/material'
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return <AppBar>
+      <Toolbar>
+        <Typography variant="h4">Save Me</Typography>
+        <Box>
+            <Button color="warning">Login</Button>
+            <Button color="warning">Signup</Button>
+        </Box>
+      </Toolbar>
+  </AppBar>;
 }
 
 export default Header
