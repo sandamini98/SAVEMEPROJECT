d7e77bf47054e6f4d7a9a5cd348c3b19b802127b
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-17 16:26:18 +0530

    re write code

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,10 @@
+/*
 import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import {AppBar, Toolbar, Typography, Box, Button,  Tabs, Tab} from '@mui/material'
 const Header = () => {
+  
+  
     const [value, setValue] = useState();
     return (
     <AppBar 
@@ -53,3 +56,107 @@ const Header = () => {
 };
 
 export default Header
+*/
+
+
+import React, { useState } from "react";
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Box,
+  Button,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+
+  const [value, setValue] = useState();
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        background:
+          "#6ec23d ",
+      }}
+    >
+      <Toolbar>
+        <Typography  variant="h4">
+          Save Me
+        </Typography>
+        {isLoggedIn && (
+          <Box display="flex" marginLeft={"auto"} marginRight="auto">
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab
+                
+                LinkComponent={Link}
+                to="/blogs"
+                label="All Blogs"
+              />
+              <Tab
+                
+                LinkComponent={Link}
+                to="/myBlogs"
+                label="My Blogs"
+              />
+              <Tab
+                
+                LinkComponent={Link}
+                to="/blogs/add"
+                label="Add Blog"
+              />
+            </Tabs>
+          </Box>
+        )}
+        <Box display="flex" marginLeft="auto">
+          {!isLoggedIn && (
+            <>
+              {" "}
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Login
+              </Button>
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Signup
+              </Button>
+            </>
+          )}
+          {isLoggedIn && (
+            <Button
+              
+              LinkComponent={Link}
+              to="/auth"
+              variant="contained"
+              sx={{ margin: 1, borderRadius: 10 }}
+              color="warning"
+            >
+              Logout
+            </Button>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
