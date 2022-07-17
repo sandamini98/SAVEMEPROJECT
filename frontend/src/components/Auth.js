53afd28b06f929a7617fc9af53949df2d82f4955
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-17 16:36:00 +0530

    add login form basic things

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,9 +1,22 @@
+import { Button, TextField, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react'
 
 const Auth = () => {
     return (
-        <div>Auth</div>
-    )
-}
+        <div>
+           <form>
+             <Box>
+                <Typography>Login</Typography>
+                <TextField/>
+                <TextField/>
+                <TextField/>
+                <Button>Submit</Button>
+                <Button>Change To Signup</Button>
+             </Box>
+           </form>
+        </div>
+    );
+};
 
 export default Auth;
