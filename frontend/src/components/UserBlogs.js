adbc6551390472ba523a9131a28608240c89f519
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-18 01:11:40 +0530

    cording my blogs and all blogs

--- frontend/src/components/UserBlogs.js
+++ frontend/src/components/UserBlogs.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Blog from "./Blog";
 const UserBlogs = () => {
+  const [blogs, setBlogs] = useState()
+  const id = localStorage.getItem("userId");
+  const sendRequest = async()=> {
+    const res = await axios.get(`http://localhost:5000/api/blog/user/${id}`).catch(err=>console.log(err))
+    const data = await res.data;
+    return data
+  }
+  useEffect(()=> {
+    sendRequest().then((data)=>setBlogs(data.blogs.blogs));
+  },[])
+  console.log(blogs);
   return (
-    <div>UserBlogs</div>
-  )
+    <div>
+      {blogs && blogs.map((blog,index)=><Blog title={blog.title} description={blog.description} imageURL={blog.imageURL} userName={blog.user.name}/>)}
+    </div>
+  );
 }
 
 export default UserBlogs;
