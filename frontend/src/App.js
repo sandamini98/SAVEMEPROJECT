772392e8db4c770e12c65cdf2b93fef0852834b6
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-17 12:56:26 +0530

    Login.js rename to Auth and import components file into the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
 import Header from "./components/Header";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetail from "./components/BlogDetail";
+import AddBlog from "./components/AddBlog";
 
 function App() {
-  return <Header/>;
+  return <React.Fragment>
+    <header>
+      <Header/>
+    </header>
+    <main>
+      <Routes>
+        <Route path="/auth" element={<Auth/>}/>
+        <Route path="/blogs" element={<Blogs/>}/>
+        <Route path="/myBlogs" element={<UserBlogs/>}/>
+        <Route path="/myBlogs/:id" element={<BlogDetail/>}/>
+        <Route path="/myBlogs/add" element={<AddBlog/>}/>
+      </Routes>
+    </main>
+
+
+
+  </React.Fragment>;
 }
 
 export default App;
--- frontend/src/components/UserBlogs.js
+++ frontend/src/components/UserBlogs.js
@@ -6,4 +6,4 @@ const UserBlogs = () => {
   )
 }
 
-export default UserBlogs
+export default UserBlogs;
