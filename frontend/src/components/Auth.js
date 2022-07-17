b0088f7c10bfa5988c8ffc13f88d3527958249b1
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-17 18:32:34 +0530

    create login function

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,18 +1,50 @@
 import { Button, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react'
+import React, { useState } from 'react'
 
 const Auth = () => {
+    const [inputs, setInputs] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+    const [isSignup, setIsSignup] = useState(false)
+    const handleChange = (e) =>{
+        setInputs((prevState)=>({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
     return (
         <div>
            <form>
-             <Box>
-                <Typography>Login</Typography>
-                <TextField/>
-                <TextField/>
-                <TextField/>
-                <Button>Submit</Button>
-                <Button>Change To Signup</Button>
+             <Box 
+                maxWidth={400}
+                display="flex" 
+                flexDirection={'column'} 
+                alignItems='center' 
+                justifyContent={'center'}
+                boxShadow="10px 10px 20px #ccc"
+                padding={3}
+                margin='auto'
+                marginTop={5}
+                borderRadius={5}
+             >
+                <Typography variant='h2' padding={3} textAlign='center'>
+                 {isSignup ? "Signup": "Login"}
+                </Typography>
+                { isSignup && <TextField name="name" onChange={handleChange} value={inputs.name} placeholder="Name" margin="normal"/>} {" "}
+                <TextField name="email" onChange={handleChange} value={inputs.email} type={'email'} placeholder="Email" margin="normal"/>
+                <TextField name="password" onChange={handleChange} value={inputs.password} type={"password"} placeholder="Password" margin="normal"/>
+                <Button variant='contained' sx={{borderRadius:3,marginTop:3}} color="warning">
+                 Submit
+                </Button>
+                <Button 
+                    onClick={()=>setIsSignup(!isSignup)} 
+                    sx={{borderRadius:3,marginTop:3}}
+                >
+                    Change To {isSignup ? "Login" :"Signup"}
+                </Button>
              </Box>
            </form>
         </div>
