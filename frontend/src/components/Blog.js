f8feb157fd1d143974e3407ea51fd5a995235076
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-17 23:27:06 +0530

    cording card part

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -3,7 +3,21 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div> <Card sx={{ maxWidth: 345 }}>
+    <div> 
+       
+       {" "}
+      <Card
+        sx={{
+          width: "40%",
+          margin: "auto",
+          mt: 2,
+          padding: 2,
+          boxShadow: "5px 5px 10px #ccc",
+          ":hover": {
+            boxShadow: "10px 10px 20px #ccc",
+          },
+        }}
+      >
     <CardHeader
       avatar={
         <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
