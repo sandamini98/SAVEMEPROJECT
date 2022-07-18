a98dbafbe405aa5c5a5a88002fc776437808fb91
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-18 16:21:39 +0530

    recheck codes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -163,7 +163,7 @@ const Header = () => {
 export default Header;
 */
 
-
+/*
 import React, { useState } from "react";
 import {
   AppBar,
@@ -220,6 +220,115 @@ const Header = () => {
                 LinkComponent={Link}
                 to="/blogs/add"
                 label="Add Blog"
+              />
+            </Tabs>
+          </Box>
+        )}
+        <Box display="flex" marginLeft="auto">
+          {!isLoggedIn && (
+            <>
+              {" "}
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Login
+              </Button>
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Signup
+              </Button>
+            </>
+          )}
+          {isLoggedIn && (
+            <Button
+              onClick={() => dispath(authActions.logout())}
+              LinkComponent={Link}
+              to="/auth"
+              variant="contained"
+              sx={{ margin: 1, borderRadius: 10 }}
+              color="warning"
+            >
+              Logout
+            </Button>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
+*/
+
+
+
+
+
+import React, { useState } from "react";
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Box,
+  Button,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store";
+import { useStyles } from "./utils";
+const Header = () => {
+  const classes = useStyles();
+  const dispath = useDispatch();
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+
+  const [value, setValue] = useState();
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        background:
+          " #6ec23d",
+      }}
+    >
+      <Toolbar>
+        <Typography className={classes.font} variant="h4">
+          Save Me
+        </Typography>
+        {isLoggedIn && (
+          <Box display="flex" marginLeft={"auto"} marginRight="auto">
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/blogs"
+                label="All Posts"
+              />
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/myBlogs"
+                label="My Posts"
+              />
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/blogs/add"
+                label="Add Post"
               />
             </Tabs>
           </Box>
