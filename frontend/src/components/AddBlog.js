98bc40cd78cb3ec4c254e4a0f155aa640de170cc
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-18 10:47:15 +0530

    add basic title,description and url inputs

--- frontend/src/components/AddBlog.js
+++ frontend/src/components/AddBlog.js
@@ -1,8 +1,19 @@
+import { Box, InputLabel, TextField, Typography } from '@mui/material';
 import React from 'react'
 
 const AddBlog = () => {
   return <div>
-      
+      <form >
+        <Box>
+          <Typography>Post Your Blog</Typography>
+          <InputLabel>Title</InputLabel>
+          <TextField/>
+          <InputLabel>Description</InputLabel>
+          <TextField/>
+          <InputLabel>ImageURL</InputLabel>
+          <TextField/>
+        </Box>
+      </form>
   </div>;
 };
 
