7064bd51bb34840df11e71125a52fe2e38cbbdd4
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-07-18 16:04:59 +0530

    add roboto font, add utils.js file, add styles

--- frontend/src/components/UserBlogs.js
+++ frontend/src/components/UserBlogs.js
@@ -1,8 +1,9 @@
+/*
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import Blog from "./Blog";
 const UserBlogs = () => {
-  const [blogs, setBlogs] = useState()
+  const [user, setUser] = useState()
   const id = localStorage.getItem("userId");
   const sendRequest = async()=> {
     const res = await axios.get(`http://localhost:5000/api/blog/user/${id}`).catch(err=>console.log(err))
@@ -10,14 +11,101 @@ const UserBlogs = () => {
     return data
   }
   useEffect(()=> {
-    sendRequest().then((data)=>setBlogs(data.blogs.blogs));
+    sendRequest().then((data)=>setUser(data.user));
   }, []);
-  console.log(blogs);
+  console.log(user);
   return (
     <div>
-      {blogs && blogs.map((blog,index)=><Blog title={blog.title} description={blog.description} imageURL={blog.imageURL} userName={blog.user.name}/>)}
+      {blogs && blogs.map((blog,index)=><Blog title={blog.title} description={blog.description} imageURL={blog.image} userName={blog.user.name}/>)}
     </div>
   );
 }
+
+export default UserBlogs;
+*/
+
+
+/*
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Blog from "./Blog";
+const UserBlogs = () => {
+  const [user, setUser] = useState();
+  const id = localStorage.getItem("userId");
+  const sendRequest = async () => {
+    const res = await axios
+      .get(`http://localhost:5000/api/blog/user/${id}`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  useEffect(() => {
+    sendRequest().then((data) => setUser(data.user));
+  }, []);
+  console.log(user);
+  return (
+    <div>
+      {" "}
+      {user &&
+        user.blogs &&
+        user.blogs.map((blog, index) => (
+          <Blog
+            id={blog._id}
+            key={index}
+            isUser={true}
+            title={blog.title}
+            description={blog.description}
+            imageURL={blog.image}
+            userName={user.name}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default UserBlogs;
+*/
+
+
+
+
+
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Blog from "./Blog";
+const UserBlogs = () => {
+  const [user, setUser] = useState();
+  const id = localStorage.getItem("userId");
+  const sendRequest = async () => {
+    const res = await axios
+      .get(`http://localhost:5000/api/blog/user/${id}`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  useEffect(() => {
+    sendRequest().then((data) => setUser(data.user));
+  }, []);
+  console.log(user);
+  return (
+    <div>
+      {" "}
+      {user &&
+        user.blogs &&
+        user.blogs.map((blog, index) => (
+          <Blog
+            id={blog._id}
+            key={index}
+            isUser={true}
+            title={blog.title}
+            description={blog.description}
+            imageURL={blog.image}
+            userName={user.name}
+          />
+        ))}
+    </div>
+  );
+};
 
 export default UserBlogs;
